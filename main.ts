9a65eedf0f6984d9e94541d2fc518e1218768105
Xx_ShuGO_xX <st22131sh@gm.ibaraki-ct.ac.jp>
2024-06-11 12:03:51 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 123456789) {
+        basic.showString("OK")
+    }
+    if (receivedNumber == 1) {
+        pins.servoWritePin(AnalogPin.P1, 0)
+    }
+    if (receivedNumber == 2) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+    }
+    if (receivedNumber == 3) {
+        pins.servoWritePin(AnalogPin.P1, 0)
+        pins.servoWritePin(AnalogPin.P2, 180)
+    }
 })
+radio.setGroup(9)
